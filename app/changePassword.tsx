320004ee394ed85659108be88f81a3ef259da987
André Gustavo <andregustavo147@gmail.com>
2025-03-03 19:42:08 -0300

    New feature change password

--- app/changePassword.tsx
+++ app/changePassword.tsx
@@ -1,8 +1,46 @@
 import { HighlightButton } from "@/components/Button/Button";
 import InputText from "@/components/InputText/InputText";
-import { View, StyleSheet } from "react-native";
+import useOfflineStorage, { User } from "@/constants/useOfflineStorage";
+import { useGlobalSearchParams, useRouter } from "expo-router";
+import { useState } from "react";
+import { View, StyleSheet, Alert } from "react-native";
+
+type HomeParams = {
+  userID: string;
+};
 
 export default function changePassword() {
+  const [user, setUser] = useState<User | null>(null);
+  const params = useGlobalSearchParams<HomeParams>();
+  const userID = params.userID;
+  const [password, setPassword] = useState<string>("");
+  const [newPassword, setNewPassword] = useState<string>("");
+  const { getUserById, updateUser, isLoading, error } = useOfflineStorage();
+  const router = useRouter();
+
+  const handleUpdateProfile = async () => {
+    try {
+      if (!userID) throw new Error("ID do usuario nao encontrado");
+      const user = await getUserById(userID);
+
+      if (user?.password !== password) throw new Error("Senha atual incorreta");
+
+      await updateUser(userID, {
+        password: newPassword,
+      });
+
+      const updatedUser = await getUserById(userID);
+      setPassword("");
+      setNewPassword("");
+
+      setUser(updatedUser);
+    } catch (error) {
+      Alert.alert("Erro ao salvar", String(error));
+    } finally {
+      router.back();
+    }
+  };
+
   return (
     <View style={styles.container}>
       <View style={styles.formContainer}>
@@ -12,13 +50,21 @@ export default function changePassword() {
           password={true}
           autoCapitalize="none"
           obrigatorio={true}
+          value={password}
+          onChangeText={(text) => {
+            setPassword(text);
+          }}
         />
         <InputText
-          label="Senha"
+          label="Nova Senha"
           placeholder="Digite a sua nova senha"
           password={true}
           autoCapitalize="none"
           obrigatorio={true}
+          value={newPassword}
+          onChangeText={(text) => {
+            setNewPassword(text);
+          }}
         />
         <InputText
           label="Confirmar senha"
@@ -30,7 +76,7 @@ export default function changePassword() {
         <HighlightButton
           label="Salvar"
           style={styles.saveButton}
-          onPress={() => {}}
+          onPress={handleUpdateProfile}
         />
       </View>
     </View>
@@ -49,6 +95,6 @@ const styles = StyleSheet.create({
     gap: 22,
   },
   saveButton: {
-    marginTop: "45%"
-  }
+    marginTop: "45%",
+  },
 });
