3ab4b6590343e65e8e268535fe116726ea14fcef
André Gustavo <andregustavo147@gmail.com>
2025-03-08 18:46:54 -0300

    Create ToastMessage.tsx

---
+++ components/ToastMessage/ToastMessage.tsx
@@ -0,0 +1,25 @@
+import { MaterialIcons } from "@expo/vector-icons";
+import { View, Text } from "react-native";
+import Toast from "react-native-toast-message"
+
+export const toastConfig = {
+    error: ({text1, text2}: any) => (
+        <View style={{
+            backgroundColor: "red",
+            padding: 15,
+            borderRadius: 10,
+            margin: 10,
+            flexDirection: "row",
+            alignItems: "center"
+        }}>
+            <MaterialIcons name="error-outline" size={24} color="white" />
+            <View style={{marginLeft: 10}}>
+                <Text style={{color: "white", fontWeight: "bold"}}>{text1}</Text>
+                <Text style={{color: "white"}}>{text2}</Text>
+            </View>
+
+        </View>
+    )
+}
+
+export default Toast
