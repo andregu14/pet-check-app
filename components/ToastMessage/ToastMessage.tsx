ec9360be2291ce21cb0a82d122c85c3cf8a0f42a
André Gustavo <andregustavo147@gmail.com>
2025-03-17 18:04:35 -0300

    Update ToastMessage.tsx
    Create new toast style: Success

--- components/ToastMessage/ToastMessage.tsx
+++ components/ToastMessage/ToastMessage.tsx
@@ -1,25 +1,44 @@
 import { MaterialIcons } from "@expo/vector-icons";
 import { View, Text } from "react-native";
-import Toast from "react-native-toast-message"
+import Toast from "react-native-toast-message";
 
 export const toastConfig = {
-    error: ({text1, text2}: any) => (
-        <View style={{
-            backgroundColor: "red",
-            padding: 15,
-            borderRadius: 10,
-            margin: 10,
-            flexDirection: "row",
-            alignItems: "center"
-        }}>
-            <MaterialIcons name="error-outline" size={24} color="white" />
-            <View style={{marginLeft: 10}}>
-                <Text style={{color: "white", fontWeight: "bold"}}>{text1}</Text>
-                <Text style={{color: "white"}}>{text2}</Text>
-            </View>
+  error: ({ text1, text2 }: any) => (
+    <View
+      style={{
+        backgroundColor: "red",
+        padding: 15,
+        borderRadius: 10,
+        margin: 10,
+        flexDirection: "row",
+        alignItems: "center",
+      }}
+    >
+      <MaterialIcons name="error-outline" size={24} color="white" />
+      <View style={{ marginLeft: 10 }}>
+        <Text style={{ color: "white", fontWeight: "bold" }}>{text1}</Text>
+        <Text style={{ color: "white" }}>{text2}</Text>
+      </View>
+    </View>
+  ),
+  success: ({ text1, text2 }: any) => (
+    <View
+      style={{
+        backgroundColor: "#4CAF50",
+        padding: 15,
+        borderRadius: 10,
+        margin: 10,
+        flexDirection: "row",
+        alignItems: "center",
+      }}
+    >
+      <MaterialIcons name="check-circle-outline" size={24} color="white" />
+      <View style={{ marginLeft: 10 }}>
+        <Text style={{ color: "white", fontWeight: "bold" }}>{text1}</Text>
+        <Text style={{ color: "white" }}>{text2}</Text>
+      </View>
+    </View>
+  ),
+};
 
-        </View>
-    )
-}
-
-export default Toast
+export default Toast;
