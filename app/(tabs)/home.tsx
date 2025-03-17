0627207dc8d31e65db654cbe70b4eb6bb7a1ba5b
André Gustavo <andregustavo147@gmail.com>
2025-03-17 18:08:24 -0300

    Update home.tsx
    Separei as listas de pet por tipo

--- app/(tabs)/home.tsx
+++ app/(tabs)/home.tsx
@@ -1,20 +1,47 @@
 import {
   ImageBackground,
   StyleSheet,
-  View,
   Text,
   FlatList,
+  ScrollView,
+  Platform,
 } from "react-native";
 import { StatusBar } from "expo-status-bar";
 import PetCard from "@/components/PetCard/PetCard";
 import useOfflineStorage, { User } from "@/constants/useOfflineStorage";
 import { useCallback, useEffect, useState } from "react";
 import { useFocusEffect, useGlobalSearchParams } from "expo-router";
-
+import Toast from "react-native-toast-message";
+import { toastConfig } from "@/components/ToastMessage/ToastMessage";
+import ListHeader from "@/components/ListHeader/ListHeader";
 const backgroundImage = require("@/assets/images/bg.jpg");
 
 type HomeParams = {
   userID: string;
+};
+
+const calcIdade = (dataNascimento: string): number => {
+  const partes = dataNascimento.split("/");
+  const diaNasc = parseInt(partes[0]);
+  const mesNasc = parseInt(partes[1]) - 1;
+  const anoNasc = parseInt(partes[2]);
+
+  const dataNasc = new Date(anoNasc, mesNasc, diaNasc);
+  const hoje = new Date();
+
+  let idade = hoje.getFullYear() - dataNasc.getFullYear();
+
+  const mesAtual = hoje.getMonth();
+  const diaAtual = hoje.getDate();
+
+  if (
+    mesAtual < dataNasc.getMonth() ||
+    (mesAtual === dataNasc.getMonth() && diaAtual < dataNasc.getDate())
+  ) {
+    idade--;
+  }
+
+  return idade;
 };
 
 export default function Home() {
@@ -23,6 +50,9 @@ export default function Home() {
   const [user, setUser] = useState<User | null>(null);
 
   const { getUserById, isLoading, error } = useOfflineStorage();
+
+  const cats = user?.pets.filter((pet) => pet.tipo === "gato") || [];
+  const dogs = user?.pets.filter((pet) => pet.tipo === "cachorro") || [];
 
   useFocusEffect(
     useCallback(() => {
@@ -43,11 +73,12 @@ export default function Home() {
   }, [userID]);
 
   if (error) {
-    return (
-      <View style={styles.errorContainer}>
-        <Text style={styles.errorText}>{error}</Text>
-      </View>
-    );
+    return Toast.show({
+      type: "error",
+      text1: "Erro ao criar conta",
+      text2: error.toString(),
+      visibilityTime: 3000,
+    });
   }
 
   return (
@@ -57,27 +88,62 @@ export default function Home() {
       resizeMode="repeat"
       imageStyle={{ opacity: 0.8 }}
     >
-      <FlatList
-        data={user?.pets}
-        onRefresh={() => getUserById(userID)}
-        refreshing={isLoading}
-        renderItem={({ item }) => (
-          <PetCard
-            key={item.id}
-            name={item.name}
-            raca={item.raca}
-            description={item.description}
-            image={item.foto}
-          />
-        )}
-        keyExtractor={(item) => item.id}
-        contentContainerStyle={[styles.container, {paddingBottom: 40}]}
-        style={styles.flatList}
-        ListEmptyComponent={
-          <Text style={styles.noPetsText}>Nenhum pet cadastrado</Text>
-        }
-      />
+      <ScrollView contentContainerStyle={styles.scrollContainer}>
+        {/* Gatos */}
+        <FlatList
+          data={cats}
+          scrollEnabled={false}
+          ListHeaderComponent={<ListHeader image="cat" title="GATOS"/>}
+          ListHeaderComponentStyle={{ width: "100%", paddingTop: Platform.OS === 'android' ? 10 : 0 }}
+          renderItem={({ item }) => (
+            <PetCard
+              key={item.id}
+              name={item.name}
+              raca={item.raca}
+              description={item.description}
+              image={item.foto}
+              idade={item.nascimento ? calcIdade(item.nascimento) : undefined }
+              sexo={item.sexo ? item.sexo : undefined}
+            />
+          )}
+          keyExtractor={(item) => item.id}
+          contentContainerStyle={styles.listContainer}
+          ListEmptyComponent={
+            <Text style={styles.noPetsText}>Nenhum pet cadastrado</Text>
+          }
+          initialNumToRender={3}
+          maxToRenderPerBatch={3}
+          windowSize={5}
+        />
+        {/* Cachorros */}
+        <FlatList
+          data={dogs}
+          scrollEnabled={false}
+          ListHeaderComponent={<ListHeader image="dog" title="CACHORROS" />}
+          ListHeaderComponentStyle={{ width: "100%" }}
+          renderItem={({ item }) => (
+            <PetCard
+              key={item.id}
+              name={item.name}
+              raca={item.raca}
+              description={item.description}
+              image={item.foto}
+              idade={item.nascimento ? calcIdade(item.nascimento) : undefined }
+              sexo={item.sexo ? item.sexo : undefined}
+            />
+          )}
+          keyExtractor={(item) => item.id}
+          contentContainerStyle={[styles.listContainer, { paddingTop: 60 }]}
+          ListEmptyComponent={
+            <Text style={styles.noPetsText}>Nenhum pet cadastrado</Text>
+          }
+          initialNumToRender={3}
+          maxToRenderPerBatch={3}
+          windowSize={5}
+        />
+      </ScrollView>
       <StatusBar backgroundColor="#fff" style="auto" />
+      <Toast config={toastConfig} />
     </ImageBackground>
   );
 }
@@ -86,14 +152,13 @@ const styles = StyleSheet.create({
   backgroundImage: {
     flex: 1,
   },
-  container: {
-    gap: 40,
-    paddingTop: 70,
-    paddingBottom: 40,
+  listContainer: {
+    gap: 50,
+    paddingTop: 33,
     alignItems: "center",
   },
   scrollContainer: {
-    flexGrow: 1,
+    paddingBottom: 40,
   },
   loadingContainer: {
     flex: 1,
@@ -115,10 +180,11 @@ const styles = StyleSheet.create({
     fontSize: 16,
     color: "#666",
     marginTop: 20,
-    textAlign: "center"
+    textAlign: "center",
+    paddingVertical: 100,
   },
   flatList: {
     flex: 1,
-    width: "100%"
-  }
+    width: "100%",
+  },
 });
